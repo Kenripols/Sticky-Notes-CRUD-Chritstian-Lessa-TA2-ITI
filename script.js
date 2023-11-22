ce1591db4ce5eed6aa77e69aa6298880760ad471
Kenripols <ken.rip2@gmail.com>
2023-11-22 15:45:34 -0300

    Sticky Notes CSS agregado y funcional

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ submitButton.addEventListener('click', function () {
     notacontent: notacontent.value,
   };
 
-  // Hacer una copia del array antes de agregar la nueva nota
+  
   let updatedNotas = [...notas];
   updatedNotas.push(nota);
 
@@ -38,8 +38,8 @@ function showNotes() {
       <div class="notestyle">
         <h2>${nota.notatitle}</h2>
         <p>${nota.notacontent}</p>
-        <button onclick="editNote(${index})">Editar</button>
-        <button onclick="deleteNote(${index})">Borrar</button>
+        <button class="botoneditar" onclick="editNote(${index})">Editar</button>
+        <button class="botonborrar" onclick="deleteNote(${index})">Borrar</button>
       </div>
     `;
   });
@@ -50,8 +50,8 @@ function showNotes() {
 function editNote(index) {
   let _notas = JSON.parse(localStorage.getItem('nota')) || [];
 
-  let updatedTitle = prompt('Ingrese el nuevo nombre:', _notas[index].notatitle);
-  let updatedContent = prompt('Ingrese el nuevo precio:', _notas[index].notacontent);
+  let updatedTitle = prompt('Ingrese el nuevo titulo:', _notas[index].notatitle);
+  let updatedContent = prompt('Ingrese el nuevo contenido:', _notas[index].notacontent);
 
   if (updatedTitle !== null && updatedContent !== null) {
     _notas[index].notatitle = updatedTitle;
@@ -65,7 +65,7 @@ function editNote(index) {
 function deleteNote(index) {
   let _notas = JSON.parse(localStorage.getItem('nota')) || [];
 
-  // Hacer una copia del array antes de eliminar la nota
+  
   let updatedNotas = [..._notas];
   updatedNotas.splice(index, 1);
 
